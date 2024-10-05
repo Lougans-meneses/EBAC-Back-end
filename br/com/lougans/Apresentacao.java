97f7adbba7dcb6c169208fc0d28a61be23dc086e
lougans meneses <lougans_meneses@hotmail.com>
2024-10-05 17:30:49 -0300

    commit inicial

---
+++ br/com/lougans/Apresentacao.java
@@ -0,0 +1,7 @@
+public class Apresentacao {
+    public static void main(String[] args) {
+        String nome = "Lougans";
+        String saudacao = "Seja muito bem vindo";
+        System.out.println(saudacao + " " + nome + " :)");
+    }
+}
