feeda79e8fae8942f09d2db8389d8a893e3b91d4
lougans meneses <lougans_meneses@hotmail.com>
2024-11-23 23:16:30 -0300

    commit inicial

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int nota1 = 5;
+        int nota2 = 8;
+        int nota3 = 9;
+        int nota4 = 10;
+
+        int media = (nota1 + nota2 + nota3 + nota4) / 4;
+        System.out.println("A média das notas são: " + media);
+    }
+}
